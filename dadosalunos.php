4c3e105b8bf7d0d2d5bb4efe6ef0eeecdd9c245b
miguelmello123 <116005283+miguelmello123@users.noreply.github.com>
2022-10-17 11:35:34 -0300

    Add files via upload

---
+++ dadosalunos.php
@@ -0,0 +1,15 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Dados Alunos</title>
+</head>
+<body>
+    <?php
+        echo "Seguem abaixo as informações digitadas na pániga anterior: "."<br><br>";
+        echo "Nome Completo: ".$_POST['txtnome']." ".$_POST['txtsobrenome']."<br>";
+        echo "Telefone: ".$_POST['txttel']."<br>";
+    
+    ?>
+</body>
+</html>
